4a937e7d716d74fc3edb10cf9fcf584275e6a4f0
bisworaj <bisworajwolvy1000@gmail.com>
2021-08-31 08:05:48 +0530

    initial messege

---
+++ client/app/components/MainContainer.js
@@ -0,0 +1,8 @@
+import { connect } from "react-redux";
+import Main from "./Main.jsx";
+
+const mapStateToProps = ({}) => ({});
+
+const mapDispatchToProps = () => (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
